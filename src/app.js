2bdcfa04833c90f41429b07b3a0b6ea510428fe0
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-11 13:10:37 -0400

    put post delete demo

--- src/app.js
+++ src/app.js
@@ -1,22 +1,93 @@
 const express = require('express')
 const app = express()
+const User = require('./models/User')
 
+app.use(express.json())
 app.use('/static', express.static('public'))
 
-app.get('/', (req, res) => {
-	const msg = `This was ${req.method} request from a browser.`
-	res.send(msg)
+app.post('/', async (req, res, next) => {
+	try {
+		const user = await User.create(req.body)
+
+		if (!user) {
+			throw new Error('No user created')
+		}
+		res.send(user)
+	} catch (error) {
+		next(error)
+	}
 })
 
-function controller(req, res) {
-	const data = {
-		method: req.method,
-		num: Math.random(),
-		str: 'Hello World!'
-	}
-	res.json(data)
-}
+app.get('/', async (req, res, next) => {
+	try {
+		const user = await User.findAll()
 
-app.get('/unicorn', controller)
+		if (!user) {
+			throw new Error('No user found')
+		}
+
+		res.send(user)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.get('/:username', async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: {
+				username: req.params.username
+			}
+		})
+
+		if (!user) {
+			throw new Error('No user found')
+		}
+
+		res.send(user)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.put('/:username', async (req, res, next) => {
+	try {
+		const updated = await User.update(req.body, {
+			where: {
+				username: req.params.username
+			}
+		})
+
+		console.log(updated)
+
+		if (updated[0] === 0) {
+			throw new Error('No update made')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.delete('/:username', async (req, res, next) => {
+	try {
+		const deleted = await User.destroy({
+			where: {
+				username: req.params.username
+			}
+		})
+
+		console.log(deleted)
+
+		if (deleted === 0) {
+			throw new Error('No user deleted')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
+	}
+})
 
 module.exports = app
---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const { db, DataTypes, Model } = require('../../db/connection.js')
+
+class User extends Model {}
+
+User.init(
+	{
+		username: DataTypes.STRING,
+		email: DataTypes.STRING,
+		password: DataTypes.STRING
+	},
+	{
+		sequelize: db,
+		modelName: 'User'
+	}
+)
+
+module.exports = User
